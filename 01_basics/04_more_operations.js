5ba5993dff0c64ecdda2ea5dd618bfd544785381
Aryan Sahu <154048519+aryaniscoding@users.noreply.github.com>
2025-02-21 15:36:39 +0000

    more conversion

---
+++ 01_basics/04_more_operations.js
@@ -0,0 +1,14 @@
+let value = 112
+let negValue = -value
+console.log(negValue)
+
+//concat
+let str1 = "aryan"
+let str2 = " sahu"
+console.log(str1 + str2)
+
+// conversion
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
