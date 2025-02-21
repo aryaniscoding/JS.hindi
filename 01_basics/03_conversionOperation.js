7dc124653e97a2e192d18dc57158701e10f066f7
Aryan Sahu <154048519+aryaniscoding@users.noreply.github.com>
2025-02-21 15:17:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = "abcdefg"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1;false =>0
+
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+let boolean_isLoggedIn = Boolean(isLoggedIn)
+
+console.log(boolean_isLoggedIn)
